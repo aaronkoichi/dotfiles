461cb0b31121a569b36325847549124ea414f1fc
aaronkoichi <aaronlee4399@gmail.com>
2024-10-03 08:26:40 +0800

    Changed polybar config and bspwm

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,9 @@ killall -q polybar
 # polybar-msg cmd quit
 #
 # launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar bar3 2>&1 | tee -a /tmp/polybar3.log & disown
 
 echo "Bars launched..."
